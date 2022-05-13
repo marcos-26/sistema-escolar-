590a89b8430af634a47ef39e235e04514e35de6f
Marcos Paulo Rodrigues <marcos.paulo.rr@outlook.com>
2022-05-13 13:12:33 -0300

    repositorio sistema escolar

---
+++ rodape.php
@@ -0,0 +1,9 @@
+<footer>
+	<div class="navbar navbar-default navbar-fixed-bottom" style="background-color: #ececec;">
+	<div class="panel-heading">
+		<div align="center" >
+			Copyright &copy <?php echo Date("Y");?> Todos Direitos Reservados a Escola Rei dos Reis- Beira
+		</div>
+	</div>
+	</div>
+</footer>
